1ffe436b94af2a1be82c5f60ab5c267f91895ac3
Luca Iachini <luca.iachini@prima.it>
2023-11-24 17:23:03 +0100

    add red center

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::{path::PathBuf, thread, time::Duration};
+use std::{fmt::Alignment, path::PathBuf, thread, time::Duration};
 
 use clap::{Parser, ValueHint};
 use document::Document;
-
 mod document;
+use tui::{
+    backend::CrosstermBackend,
+    style::{Color, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, Paragraph},
+    Terminal,
+};
 
 #[derive(Parser)]
 struct Args {
@@ -14,21 +20,49 @@ struct Args {
 }
 
 fn parse_speed(arg: &str) -> Result<std::time::Duration, std::num::ParseIntError> {
-    let seconds = arg.parse()?;
-    Ok(std::time::Duration::from_secs(seconds))
+    let millis = arg.parse()?;
+    Ok(std::time::Duration::from_millis(millis))
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    let stdout = std::io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
     let mut doc = document::EpubDoc::open(&args.path).expect("unable to open the epub");
     let mut pages = doc.pages();
     while let Some(page) = pages.next() {
-        println!("###### page {}", page.number);
         let mut words = page.words();
         while let Some(word) = words.next() {
-            println!("{word}");
+            terminal.draw(|f| {
+                let (first_half, center, second_half) = split_word(word);
+                let word_text: Spans = vec![
+                    Span::raw(first_half),
+                    Span::styled(center, Style::default().fg(Color::Red)),
+                    Span::raw(second_half),
+                ]
+                .into();
+                let widget = Paragraph::new(word_text)
+                    .block(
+                        Block::default()
+                            .title(format!("Page {}", page.number))
+                            .borders(Borders::ALL),
+                    )
+                    .style(Style::default().fg(Color::White).bg(Color::Black));
+                f.render_widget(widget, f.size());
+            })?;
             thread::sleep(args.speed);
         }
     }
     Ok(())
+}
+
+fn split_word(word: &str) -> (String, String, String) {
+    let mid = (word.len() - 1) / 2;
+    let center = word.chars().nth(mid).unwrap_or_default().to_string();
+    let first_half = word.chars().take(mid).collect();
+    let second_half = word.chars().skip(mid + 1).collect();
+    (first_half, center, second_half)
 }
